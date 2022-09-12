b530058e3f4ce789942489d6c7dc009f4c1556df
Sohail Raza <Sohailmalik059@gmail.com>
2022-09-12 11:43:17 +0500

    changes in app js

--- scripts/greeter.js
+++ scripts/greeter.js
@@ -4,10 +4,6 @@
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
 const hre = require("hardhat");
-const key = "newabc2"
-
-const ANVA = "testing"
-const code = "testing"
 
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command
--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import './App.css';
 
 function App() {
 const password = "abcuser";
+const key = "newabc2"
 
+const ANVA = "testing"
+const code = "testing"
+const codeNew = "testingNew"
   return (
     <div className="App">
 
